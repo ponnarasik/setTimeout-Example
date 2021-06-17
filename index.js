07f661dbe3e9c99cd3b8b371492c0c3861657dd5
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-06-17 14:36:25 +0000

    Saved

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function printMessage() {
+/*function printMessage() {
     console.log("The future is now!");
 }
 
 setTimeout(printMessage, 5000);
+*/
+
+setTimeout(function () {
+   console.log("The future is now!");
+}, 5000);
